c25c2c5f41f777b7cc2d2f6b17a9c50b18d8f886
yanamikha <yanamikha@gmail.com>
2025-02-24 00:39:10 +0100

    update table styles

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -2,7 +2,7 @@ import css from './TransactionHistory.module.css';
 
 export default function TransactionHistory({ items }) {
     return (
-        <table >
+        <table className={css.table}>
             <thead>
                 <tr>
                     <th>Type</th>
